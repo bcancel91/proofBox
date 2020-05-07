2e32b5c5684f14a47150b37d4b1ae3298ed36600
Brian Cancel <brian.e.cancel@gmail.com>
2020-05-07 17:14:05 -0400

    basic style for demo container

--- client/src/demo.body.js
+++ client/src/demo.body.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./demo.body.css";
+
 
 
 const DemoContainer = () => {
@@ -6,10 +8,22 @@ const DemoContainer = () => {
     return (
         <div className="container-fluid">
        <div className="row">
-         <div className="col-3">Demo Image</div>
-         <div className="col-3">Demo Image</div>
-         <div className="col-3">Demo Image</div>
-         <div className="col-3">Demo Image</div>
+         <div className="col-3 uploadBox">
+         <h2> Demo Image</h2>
+        <img src="https://via.placeholder.com/150" className="demoImg"></img>
+        </div>
+         <div className="col-3 uploadBox">
+         <h2> Demo Image</h2>
+        <img src="https://via.placeholder.com/150" className="demoImg"></img>
+         </div>
+         <div className="col-3 uploadBox">
+         <h2> Demo Image</h2>
+        <img src="https://via.placeholder.com/150" className="demoImg"></img>
+         </div>
+         <div className="col-3 uploadBox">
+         <h2> Demo Image</h2>
+        <img src="https://via.placeholder.com/150" className="demoImg"></img>
+         </div>
         </div>
         </div>
      
